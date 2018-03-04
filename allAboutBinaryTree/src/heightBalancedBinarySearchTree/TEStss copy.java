db4e15f18d5dec40522a878ea259cdda3b6f97c9
fangfangtemp <37009336+fangfangtemp@users.noreply.github.com>
2018-03-04 14:53:38 -0800

    Sun1 (#6)
    * new files
    
    * turn sat into sun
    
    * delete a row
    
    * change sat into sun in TEStss.java

--- allAboutBinaryTree/src/heightBalancedBinarySearchTree/TEStss copy.java
+++ allAboutBinaryTree/src/heightBalancedBinarySearchTree/TEStss copy.java
@@ -20,8 +20,7 @@ public class TEStss {
         }
         System.out.println(res[size - 1]);
 
-        System.out.printf("It's Sun morning");
-        System.out.printf("It's Sat morning and everything gonna be better from now on");
-
+        System.out.printf("It's Sunday morning");
+        //System.out.printf("It's Sat morning and everything gonna be better from now on");
     }
 }
